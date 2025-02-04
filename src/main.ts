ba83cfbec78d55d0023817ce553925c31e8b3898
Sylwia Przystawska <sylwiateresap@gmail.com>
2025-02-04 18:47:21 +0100

    first main push

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,10 @@
+import createcategorySelect from "./components/A/drop-down";
+import createSearchForm from "./components/A/SearchForm/SearchForm";
+//import createFruitSearchForm from "./components/Fruit/FruitSearchForm/FruitSearchForm";
 import "./style.scss";
 
+const main = document.querySelector("main") as HTMLDivElement;
+
+main.append(createcategorySelect());
+main.append(createSearchForm());
+//main.append(createFruitSearchForm())
